d750ca162e3e8daf7d8dc68bb80e86fd2d6bbf46
Dylan Simon <grvdylans@gmail.com>
2026-01-10 19:59:52 -0700

    Initial project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="w-full h-50 bg-white/20">
+            <div className="">hi</div>
+            <div className=""></div>
+        </div>
+    );
+}
