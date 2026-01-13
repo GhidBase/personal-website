8ae1ab01f3425afceee48c6ac18f1c5b692de0c5
Dylan Simon <grvdylans@gmail.com>
2026-01-13 10:56:18 -0700

    Fix animation breakpoints

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -20,21 +20,33 @@ export default function Project({ title }) {
                 
                 w-full h-full object-cover
                 transition-all duration-500
-                group-hover:blur-[2px]
+                group-focus:blur-[2px] sm:group-focus:blur-none group-hover:blur-[2px]!
                 group-hover:opacity-80
+                will-change-transform
 "
             />
 
             <a
-                id={title + "-button-container"}
                 className="
-                    absolute left-0 top-0
-                    w-full h-full
-                    flex flex-col
-                    justify-center items-center
-                    text-black"
+                    absolute inset-0 flex items-center justify-center
+                    scale-200
+                    opacity-0 group-hover:opacity-100 will-change-[opacity]
+                    transition-opacity duration-300
+  "
             >
-                Details
+                <span
+                    className="
+                        px-6 py-2
+                        rounded-full
+                        bg-black/40 backdrop-blur-md
+                        border border-white/30
+                        text-white font-medium tracking-wide
+                        hover:bg-black/60
+                        transition
+    "
+                >
+                    Details →
+                </span>
             </a>
 
             <div
@@ -44,9 +56,13 @@ export default function Project({ title }) {
                 h-40 group-focus:h-11 sm:group-focus:h-40
                 opacity-100 
                 transition-all duration-700 delay-100 sm:delay-0
-                p-2 group-hover:blur-[2px]"
+                group-focus:delay-0 group-focus:duration-400 
+                p-2 group-focus:blur-[2px] sm:group-focus:blur-none group-hover:blur-[2px]!"
             >
-                <h1 id={"p-" + title + "-title"} className="will-change-transform pb-2 text-xl">
+                <h1
+                    id={"p-" + title + "-title"}
+                    className="will-change-transform pb-2 text-xl"
+                >
                     {title}
                 </h1>
                 <div
