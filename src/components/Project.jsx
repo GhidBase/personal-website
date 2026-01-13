101b958d5f55d5756341d38b63461ac446314b22
Dylan Simon <grvdylans@gmail.com>
2026-01-12 17:09:41 -0700

    Add blur to my projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,24 +7,37 @@ export default function Project({ title }) {
             tabIndex={0}
             className="bg-zinc-700
                 w-80 h-100 md:w-150 md:h-150 relative group
-                overflow-hidden"
+                overflow-hidden
+                blur-none hover:blur-xs transition-all delay-0 duration-500"
         >
-            <img src={ldg} alt="" className="w-full h-full object-cover" />
+            <div
+                id={title + "-button-container"}
+                className="absolute left-0 top-0 w-full h-full flex flex-col justify-center items-center"
+            >
+                <a className="">See More</a>
+            </div>
+
+            <img
+                src={ldg}
+                alt=""
+                className="w-full h-full object-cover
+                opacity-100 sm:group-hover:opacity-80 transition-all delay-0 duration-500"
+            />
             <div
                 id={"p-" + title + "-detail-container"}
                 className="absolute bottom-0 left-0 right-0
                 bg-zinc-500
-                h-40 group-focus:h-10
-                opacity-100 sm:group-hover:opacity-50 group-focus:opacity-80
-                transition-all duration-700 delay-100
+                h-40 group-focus:h-11 sm:group-focus:h-40
+                opacity-100 group-focus:opacity-80 
+                transition-all duration-700 delay-100 sm:delay-0
                 p-2"
             >
-                <h1 id={"p-" + title + "-detail-container"} className="pb-2">
+                <h1 id={"p-" + title + "-title"} className="pb-2 text-xl">
                     {title}
                 </h1>
                 <div
                     id={"p-" + title + "-details"}
-                    className="min-h-0 group-focus:opacity-0 transition-all overflow-hidden duration-700"
+                    className="min-h-0 group-focus:opacity-0 sm:group-focus:opacity-100 transition-all overflow-hidden duration-500"
                 >
                     <p className="">
                         Full-stack website built using React, Express,
