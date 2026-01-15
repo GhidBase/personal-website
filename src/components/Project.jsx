646fc21257a78f057751e654d658bcefbd4fec13
Dylan Simon <grvdylans@gmail.com>
2026-01-15 11:35:49 -0700

    Simplify the project object's structure and animation

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,75 +10,74 @@ export default function Project({ title, description, imgSrc, slug }) {
             className="bg-zinc-700
                 relative group
                 overflow-hidden
-                scale-100 md:hover:scale-102! focus:scale-102 md:focus:scale-100
+                scale-100 md:hover:scale-102!
                 transition-all delay-0 duration-500
                 will-change-transform
                 shadow-lg/80
                 "
         >
-            <img
-                src={imgSrc}
-                alt=""
-                className="
-                
-                w-full h-full object-cover
-                duration-500
-                
-                transition-all blur-none group-focus:blur-[2px] md:group-focus:blur-none group-hover:blur-[2px]!
-                md:group-hover:opacity-80
-                will-change-transform
-"
-            />
-
             <div
+                id={title + "detail-button"}
                 onClick={() => navigate(slug)}
                 className="
-                    absolute inset-0 flex items-center justify-center
-                    scale-150 md:scale-200
-                    opacity-0 group-focus:opacity-100 md:group-focus:opacity-0 md:group-hover:opacity-100! will-change-[opacity]
-                    transition-opacity duration-300
-                    z-1
-  "
+                        absolute inset-0 flex items-center justify-center
+                        scale-150 md:scale-200
+                        opacity-0 md:group-hover:opacity-100! will-change-[opacity]
+                        transition-opacity duration-300
+                        z-1
+                  "
             >
                 <span
                     className="
-                        px-6 py-2
-                        rounded-full
-                        bg-black
-                        opacity-60 backdrop-blur-md
-                        border border-white/30
-                        text-white font-medium tracking-wide
-                        
-                        hover:opacity-100!
-                        transition-all duration-200
-    "
+                            px-6 py-2
+                            rounded-full
+                            bg-black
+                            opacity-60 backdrop-blur-md
+                            border border-white/30
+                            text-white font-medium tracking-wide
+                
+                            hover:opacity-100!
+                            transition-all duration-200
+                    "
                 >
                     Details →
                 </span>
             </div>
-
             <div
-                id={"p-" + title + "-detail-container"}
-                className="absolute bottom-0 left-0 right-0
-                bg-zinc-500
-                h-40
-                opacity-100 duration-700 md:delay-0
-                group-focus:delay-0 group-focus:duration-00
-                p-2 md:p-4
-                transition-all blur-none group-focus:blur-[2px] md:group-focus:blur-none group-hover:blur-[2px]!
+                id={title + "blur-container"}
+                className="
+                group
+                h-full w-full
+                transition-all
+                blur-none group-hover:blur-[2px]!
+                duration-500
+                relative
+                will-change-transform
                 "
             >
-                <h1
-                    id={"p-" + title + "-title"}
-                    className="will-change-transform pb-2 text-xl"
-                >
-                    {title}
-                </h1>
+                <img
+                    id={title + "-image"}
+                    src={imgSrc}
+                    alt=""
+                    className="
+                    w-full h-full object-cover
+                "
+                />
+
                 <div
-                    id={"p-" + title + "-details"}
-                    className="min-h-0 transition-all overflow-hidden duration-500 will-change-transform"
+                    id={"p-" + title + "-detail-container"}
+                    className="absolute bottom-0 left-0 right-0
+                    bg-zinc-500
+                    h-40
+                    p-2 md:p-4
+                    "
                 >
-                    {description}
+                    <h1 id={"p-" + title + "-title"} className="pb-2 text-xl">
+                        {title}
+                    </h1>
+                    <div id={"p-" + title + "-details"} className="">
+                        {description}
+                    </div>
                 </div>
             </div>
         </div>
