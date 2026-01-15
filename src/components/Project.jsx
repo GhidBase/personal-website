15b51bde945dfd6d0f001e9e66e9e43dbf47cffa
Dylan Simon <grvdylans@gmail.com>
2026-01-15 12:26:01 -0700

    Finish the mobile layout

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -20,10 +20,10 @@ export default function Project({ title, description, imgSrc, slug }) {
                 id={title + "detail-button"}
                 onClick={() => navigate(slug)}
                 className="
-                        pointer-events-none group-hover:pointer-events-auto
+                        pointer-events-none group-focus:pointer-events-auto md:group-hover:pointer-events-auto
                         absolute inset-0 flex items-center justify-center
                         scale-150 md:scale-200
-                        opacity-0 md:group-hover:opacity-100! will-change-[opacity]
+                        opacity-0 group-hover:opacity-100! will-change-[opacity]
                         transition-opacity duration-300
                         z-1
                   "
@@ -33,14 +33,13 @@ export default function Project({ title, description, imgSrc, slug }) {
                     className="
                             px-6 py-2
                             rounded-full
-                            bg-black
-                            opacity-60 backdrop-blur-md
+                            bg-zinc-800 md:bg-black
+                            md:opacity-60 backdrop-blur-md
                             border border-white/30
                             text-white font-medium tracking-wide
                 
                             hover:opacity-100!
-                            transition-all duration-400
-                    "
+                            transition-all duration-400"
                 >
                     Details →
                 </span>
@@ -48,7 +47,6 @@ export default function Project({ title, description, imgSrc, slug }) {
             <div
                 id={title + "blur-container"}
                 className="
-                group
                 h-full w-full
                 transition-all
                 blur-none group-hover:blur-[2px]!
@@ -57,6 +55,18 @@ export default function Project({ title, description, imgSrc, slug }) {
                 will-change-transform
                 "
             >
+                <span
+                    id={title + "-mobile-details-button"}
+                    className="
+                    absolute bottom-3 right-4
+                    text-sm text-white/70
+                    tracking-wide
+                    pointer-events-none
+                    z-1
+                    md:hidden"
+                >
+                    Details →
+                </span>
                 <img
                     id={title + "-image"}
                     src={imgSrc}
